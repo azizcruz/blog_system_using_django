543e3e83ae6935734a0d85d370e61cb9ceb04215
edu <edu@localhost.localdomain>
2019-01-01 15:56:42 +0100

    Created add post functionality with image

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
-    image = models.ImageField(upload_to='post_image', default='default.png')
+    image = models.ImageField(upload_to='post_image', default='post_image/default.png')
     publish_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True, auto_now_add=False)
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -13,4 +14,31 @@ def index(request):
     return render(request, 'blog/posts.html', context)
 
 def post_detail(request, id):
-    pass
+    query = get_object_or_404(Post, id=id)
+    date = query.publish_date.strftime('%A, %D')
+    context = {
+        'post': query,
+        'publish_date': date,
+        'title': query.title
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+def create_post(request):
+    if request.POST:
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid:
+            instance = form.save(commit=False)
+            instance.save()
+            form = PostForm()
+            context = {
+                'success': 'Added successfully.',
+                'form': form
+            }
+    else:
+        form = PostForm()
+        context = {
+            'form': form,
+            'title': 'Create Post'
+        }
+
+    return render(request, 'blog/form.html', context)
