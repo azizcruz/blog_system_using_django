43ce4b66530a26cab0fa19d864376367d9bdb7a4
edu <edu@localhost.localdomain>
2019-01-07 15:41:24 +0100

    Fixed some bugs

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
+  // Handle delete post.
+  $("#delete-post").on('click', function(e) {
+    var ans = confirm("Are you sure you want to delete this post ?")
+
+    if(!ans) {
+      e.preventDefault();
+      return false;
+    }
+
+  })
 });
